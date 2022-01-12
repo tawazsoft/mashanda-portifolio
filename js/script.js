47e7579131f7a67f6bc47afe8b1faafe387d4900
tawazsoft <72101323+tawazsoft@users.noreply.github.com>
2022-01-12 09:36:18 +0200

    11

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Android Developer",],
+    strings:["","Web Designer","Web Developer","Android Dev",],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
